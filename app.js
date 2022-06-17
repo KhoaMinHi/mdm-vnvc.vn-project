98fb0a996f0c1867c7bbf0f8a104cc0583d34a06
Huu Hoang <18120387@student.hcmus.edu.vn>
2022-06-17 14:13:55 +0700

    Vaccine List

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const ordersRouter = require('./routes/orders');
+const VaccineListRouter = require('./routes/Vaccine_list');
 var app = express();
 
 // view engine setup
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/orders', ordersRouter);
+app.use('/Vaccine_list', VaccineListRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
