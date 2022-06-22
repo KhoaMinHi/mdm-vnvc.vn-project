ae01738e680eacfb7aa5cd7d290bb7f46fb5b7ca
KhoaMinHi <minhkhoa031099@gmail.com>
2022-06-23 01:28:27 +0700

    register customer successfully, khoaminhi!

--- routes/customerRouter.js
+++ routes/customerRouter.js
@@ -6,6 +6,8 @@ router.get('/all', customer.getAll);
 router.post('/add', customer.addCustumer);
 router.get('/email/:email', customer.getByEmail);
 router.get('/id/:id', customer.getById);
-
+router.get('/register', customer.registerHtml);
+router.post('/register', customer.register);
+router.post('/activate', customer.activate);
 
 module.exports = router;
