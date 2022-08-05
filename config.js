bf18373380ff113c42c87622082f6875b0ec351f
KhoaMinHi <66743540+khoaminhi@users.noreply.github.com>
2022-08-05 07:41:28 +0700

    remove sensitive data
    password of URL_MONGODB: 'mongodb+srv://root:yourpassword@cluster0.ax9tywe.mongodb.net/DATH?retryWrites=true&w=majority' in congif.js file

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    URL_MONGODB: 'mongodb+srv://root:mdmhcmus983@cluster0.ax9tywe.mongodb.net/DATH?retryWrites=true&w=majority'
+    URL_MONGODB: 'mongodb+srv://root:yourpassword@cluster0.ax9tywe.mongodb.net/DATH?retryWrites=true&w=majority'
     //URL_MONGODB: 'mongodb://localhost:27017'
 }
